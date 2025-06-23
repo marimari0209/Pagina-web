9d616f00fe465750010eb129ef318fc5d72c7ea4
marimari0209 <MARIANA.GERALDI.OLIVEIRA@escola.pr.gov.br>
2025-06-23 11:46:54 +0000

    salvar

---
+++ arquivosJS/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Japão?',
+    'Capital é Tokio.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o principal líder da Inconfidência Mineira?',
+    'Joaquim José da Silva Xavier'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do estado de Nova Iorque? ?',
+    'A capital do estado de Nova Iorque é Albany.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os países que fazem fronteira com o Brasil?',
+    'Os países são: Guiana Francesa, Suriname, Guiana, Venezuela, Colômbia, Peru, Bolívia, Paraguai, Argentina, Uruguai.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a diferênça entre massa e peso?',
+    'A diferênça é: O peso é a força com que um corpo é atraído pela Terra. Massa é uma medida direta da oposição que um corpo oferece à mudança em seu estado de movimento.'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quais são os principais elementos de uma narrativa literária?',
+    'são: Intodução, Desenvolvimento, Clímax e Conclusão.'
+)
